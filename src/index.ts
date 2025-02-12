6419a3d87e2bae9ff453c009093940d3658e9bbe
Thierry <thierrysantoos123@gmail.com>
2025-02-12 08:18:38 -0300

    fix: opentelemetry packages conflict

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,53 @@
 import { Elysia } from 'elysia'
-import { swagger } from '@elysiajs/swagger'
 import { opentelemetry } from '@elysiajs/opentelemetry'
 
+import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-base'
 import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-node'
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto'
 import { PrismaInstrumentation } from '@prisma/instrumentation'
-
 import { user } from './user'
 import { post } from './post'
+import { Resource } from '@opentelemetry/resources'
+import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from '@opentelemetry/semantic-conventions'
+const otlpTraceExporter = new OTLPTraceExporter()
+
 
 const app = new Elysia()
+	.onBeforeHandle({as: 'global'},
+		function injectAttributes({
+			body,
+			cookie,
+			params,
+			request,
+			response,
+			route,
+			server,
+			store,
+			headers,
+			path,
+			query
+		}) {}
+	)
 	.use(
 		opentelemetry({
 			instrumentations: [new PrismaInstrumentation()],
-			spanProcessors: [new BatchSpanProcessor(new OTLPTraceExporter())]
+			spanProcessors: [new BatchSpanProcessor(otlpTraceExporter)],
+			traceExporter: otlpTraceExporter,
+			resource: new Resource({
+				[ATTR_SERVICE_NAME]: 'elysia-opentelemetry-example',
+				[ATTR_SERVICE_VERSION]: '1.0.0',
+				'deployment.environment.name': 'development',
+			}),
+			serviceName: 'elysia-opentelemetry-example',
 		})
 	)
-	.use(swagger())
+	.get('/health', () => {
+		return {
+			status: 'ok'
+		}
+	})
 	.use(user)
 	.use(post)
-	.get('/', ({ redirect }) => redirect('/swagger'))
-	.get('/health', 'ok XD')
 	.listen(3000)
 
 console.log(
